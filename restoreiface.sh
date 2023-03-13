65d3b212772a33a301479dbf87ddc0da915b97ee
Daniel Marco <damarbo33@gmail.com>
2023-03-13 12:05:28 +0100

    Create restoreiface.sh

---
+++ restoreiface.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+wanip=$(ifconfig ue0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+IDs=$(usbconfig list | grep Xiaomi | sed -r 's/ugen([0-9]\.[0-9]).*/\1/')
+
+
+if [ -z "$wanip" ]
+then
+      echo $wanip is empty >> ~/renewout.log
+     #usbconfig -d 1.2 set_config 1 ~/renewout.log
+     for ID in $IDs
+     do
+        usbconfig -d $ID set_config 1
+     done
+     dhclient ue0 >> ~/renewout.log
+#else
+#     echo $wanip is NOT empty >> ~/renewout.log
+fi
