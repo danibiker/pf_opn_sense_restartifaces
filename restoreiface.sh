c918170358babbd4f79fd8e07aabcae6726be93c
Daniel Marco <damarbo33@gmail.com>
2023-03-13 13:36:58 +0100

    Update restoreiface.sh

--- restoreiface.sh
+++ restoreiface.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+BOUNCE=ue0
 wanip=$(ifconfig ue0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+echo $wanip
 IDs=$(usbconfig list | grep Xiaomi | sed -r 's/ugen([0-9]\.[0-9]).*/\1/')
-
+DEST="www.google.es"
 
 if [ -z "$wanip" ]
 then
@@ -10,9 +12,21 @@ then
      #usbconfig -d 1.2 set_config 1 ~/renewout.log
      for ID in $IDs
      do
-        usbconfig -d $ID set_config 1
+        usbconfig -d $ID set_config 1 >> ~/renewout.log 2>> ~/renewout.log
+        usbconfig -d $ID set_config 0 >> ~/renewout.log 2>> ~/renewout.log
+        ifconfig $BOUNCE down >> ~/renewout.log 2>> ~/renewout.log
+        ifconfig $BOUNCE up >> ~/renewout.log 2>> ~/renewout.log
      done
-     dhclient ue0 >> ~/renewout.log
+     dhclient $BOUNCE >> ~/renewout.log
+
+     echo `date +%Y%m%d.%H%M%S` "Pinging $DEST" >> $LOGFILE
+     ping -c1 $DEST  >> ~/renewout.log 2>> ~/renewout.log
+     if [ $? -eq 0 ]
+     then
+        echo `date +%Y%m%d.%H%M%S` "Ping $DEST OK." >> $LOGFILE
+        service netif restart && service routing restart
+     fi
+
 #else
 #     echo $wanip is NOT empty >> ~/renewout.log
 fi
